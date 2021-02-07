33934e0f718b910bb3e4389eae7ba74e04a6ac37
tomas lingotti <tomaslingotti@gmail.com>
2021-02-07 03:37:46 -0300

    add some formatting and configs

--- tomatto/logs.go
+++ tomatto/logs.go
@@ -16,10 +16,21 @@ type tomatto struct {
 	logInfo *log.Logger
 	logWarn *log.Logger
 	logErr  *log.Logger
+	pretty  bool
+	debug   bool
+}
+
+func (t *tomatto) marshall(msg interface{}) []byte {
+	if t.pretty {
+		b, _ := json.MarshalIndent(msg, "", "  ")
+		return b
+	}
+	b, _ := json.Marshal(msg)
+	return b
 }
 
 //NewTomatto use it at the beginning of the main function
-func NewTomatto() {
+func NewTomatto(pretty, debug bool) {
 	// clean tomatto date using setFlag(0)
 	log.SetFlags(0)
 	infoLogger := log.New(os.Stdout, "", 0)
@@ -29,24 +40,32 @@ func NewTomatto() {
 		logInfo: infoLogger,
 		logWarn: warningLogger,
 		logErr:  errorLogger,
+		pretty:  pretty,
+		//debug:   debug,
 	}
 }
 
 func Info(message interface{}) {
-	msg := newTomatto(getStackTrace, message)
-	b, _ := json.MarshalIndent(msg, "", "  ")
+	msg := newTomatto(getStackTrace, message, _t.debug)
+	b := _t.marshall(msg)
 	_t.logInfo.Print(string(b))
 }
 
-func Err(message interface{}) {
-	msg := newTomatto(getStackTrace, message)
-	b, _ := json.MarshalIndent(msg, "", "  ")
-	_t.logWarn.Print(string(b))
+func Error(message interface{}) {
+	msg := newTomatto(getStackTrace, message, _t.debug)
+	b := _t.marshall(msg)
+	_t.logErr.Print(string(b))
+}
+
+func ErrorS(message string, err error) {
+	msg := newTomatto(getStackTrace, message+err.Error(), _t.debug)
+	b := _t.marshall(msg)
+	_t.logErr.Print(string(b))
 }
 
 func Warn(message interface{}) {
-	msg := newTomatto(getStackTrace, message)
-	b, _ := json.MarshalIndent(msg, "", "  ")
+	msg := newTomatto(getStackTrace, message, _t.debug)
+	b := _t.marshall(msg)
 	_t.logWarn.Print(string(b))
 }
 
@@ -56,22 +75,22 @@ func Warn(message interface{}) {
 
 func Infof(s string, values ...interface{}) {
 	formatted := fmt.Sprintf(s, values...)
-	msg := newTomatto(getStackTrace, formatted)
-	b, _ := json.MarshalIndent(msg, "", "	")
+	msg := newTomatto(getStackTrace, formatted, _t.debug)
+	b := _t.marshall(msg)
 	_t.logInfo.Print(string(b))
 }
 
 func Errorf(s string, values ...interface{}) {
 	formatted := fmt.Sprintf(s, values...)
-	msg := newTomatto(getStackTrace, formatted)
-	b, _ := json.MarshalIndent(msg, "", "	")
+	msg := newTomatto(getStackTrace, formatted, _t.debug)
+	b := _t.marshall(msg)
 	_t.logErr.Print(string(b))
 }
 
 func Warnf(s string, values ...interface{}) {
 	formatted := fmt.Sprintf(s, values...)
-	msg := newTomatto(getStackTrace, formatted)
-	b, _ := json.MarshalIndent(msg, "", "	")
+	msg := newTomatto(getStackTrace, formatted, _t.debug)
+	b := _t.marshall(msg)
 	_t.logWarn.Print(string(b))
 }
 
@@ -92,19 +111,23 @@ func getStackTrace() (uintptr, string, int, error) {
 //Message: the actual message to be logged.
 type MsgTomatto struct {
 	File     string      `json:"file"`
-	Function string      `json:"function"`
+	Function string      `json:"function,omitempty"`
 	Line     int         `json:"line"`
 	Message  interface{} `json:"message,omitempty"`
 	// fatal is out of scope
 }
 
-func newTomatto(fn func() (uintptr, string, int, error), message interface{}) *MsgTomatto {
+func newTomatto(fn func() (uintptr, string, int, error), s interface{}, debug bool) *MsgTomatto {
 	pc, file, line, _ := fn()
+	var funcCall string
+	if debug {
+		funcCall = runtime.FuncForPC(pc).Name()
+	}
 
 	return &MsgTomatto{
 		Line:     line,
-		Function: runtime.FuncForPC(pc).Name(),
+		Function: funcCall,
 		File:     path.Base(file),
-		Message:  message,
+		Message:  s,
 	}
 }
--- tomatto/logs_test.go
+++ tomatto/logs_test.go
@@ -1,23 +1,31 @@
 package tomatto
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func Test_infoLogger(t *testing.T) {
-	NewTomatto()
+	NewTomatto(false, false)
 	Info("if you can read this, is working")
 }
 
 func Test_warnLogger(t *testing.T) {
-	NewTomatto()
+	NewTomatto(true, true)
 	Warn("if you can read this, is working")
 }
 
 func Test_errLogger(t *testing.T) {
-	NewTomatto()
-	Err("if you can read this, is working")
+	NewTomatto(false, true)
+	Error("if you can read this, is working")
 }
 
 func Test_formattedInfo(t *testing.T) {
-	NewTomatto()
+	NewTomatto(true, false)
 	Infof("if you can read %s, is %s. And this is a number: %d", "this", "working", 150)
+}
+
+func Test_formattedErr(t *testing.T) {
+	NewTomatto(true, true)
+	ErrorS("if you can read ", errors.New("is working"))
 }
